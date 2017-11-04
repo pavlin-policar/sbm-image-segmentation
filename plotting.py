0652bec60068fa941947330d246e434725f79f19
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-11-04 14:57:59 +0100

    Plot image graph

--- plotting.py
+++ plotting.py
@@ -1,9 +1,25 @@
 import fire
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+
+import graph_tool.draw
 from data_provider import BSDS
+from image import image_to_graph, node2pixel
 
 sns.set('paper', 'whitegrid')
+
+
+def image_graph(image_id: str):
+    image_id = str(image_id)
+    image = BSDS.load(image_id)[:20, :20]
+    graph = image_to_graph(image, 2, np.sqrt(2), 4)
+    print(graph.vertex(43).out_degree())
+
+    pos = graph.new_vertex_property('vector<float>')
+    for node in graph.vertices():
+        pos[node] = node2pixel(int(node), image)
+    graph_tool.draw.graph_draw(graph, pos=pos)
 
 
 def true_segmentation(image_id: str):
