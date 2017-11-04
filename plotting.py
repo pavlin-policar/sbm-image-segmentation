ffeb36d55ef7d0ea2332e01c707266d1654724d4
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-11-04 17:00:42 +0100

    Implement graph block visualization

--- plotting.py
+++ plotting.py
@@ -2,10 +2,11 @@ import fire
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import matplotlib.cm as cm
 
 import graph_tool.draw
 from data_provider import BSDS
-from image import image_to_graph, node2pixel, sbm_segmentation
+from image import image_to_graph, node2pixel, sbm_segmentation, pixel2node
 
 sns.set('paper', 'whitegrid')
 
@@ -33,15 +34,18 @@ def image_partition(image_id: str):
     top, left = 100, 200
     height, width = 100, 100
     image = BSDS.load(image_id)[top:top + height, left:left + width]
-    graph = image_to_graph(image, 2, np.sqrt(2), 4)
+    graph = image_to_graph(image, 3, np.sqrt(2), 4)
 
     segmentation = sbm_segmentation(graph, image)
+    colors = graph.new_vertex_property('vector<float>')
+    for idx in np.ndindex(segmentation.shape[:2]):
+        colors[pixel2node(*idx, image)] = cm.terrain(segmentation[idx] / np.max(segmentation))
 
     positions = graph.new_vertex_property('vector<float>')
     for node in graph.vertices():
         positions[node] = reversed(node2pixel(int(node), image))
 
-    graph_tool.draw.graph_draw(graph, pos=positions, vertex_fill_color=segmentation, vertex_size=5)
+    graph_tool.draw.graph_draw(graph, pos=positions, vertex_fill_color=colors, vertex_size=5)
 
 
 def true_segmentation(image_id: str):
